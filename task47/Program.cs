ec8448a8310fc39dbbdd6a4f188b0614981aa08c
1 <bondarenko-1994alenka@mail.ru>
2022-06-07 14:20:41 +0300

    Lesson 7

---
+++ task47/Program.cs
@@ -0,0 +1,56 @@
+﻿/*Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+
+int GetNumber(string message)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        string num = Console.ReadLine();
+        if (int.TryParse(num, out int number) == false)
+            Console.WriteLine("Введено не число");
+        else
+        {
+            if (number <= 0)
+                Console.WriteLine("Задайте число больше 0");
+            else
+                return number;
+        }
+    }
+}
+
+double[,] ArrayRandom(int line, int column)
+{
+    double[,] arr = new double[line, column];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            arr[i, j] = rnd.NextDouble();
+    }
+    return arr;
+}
+
+
+
+void PrintArray(double[,] arr)
+{
+    Console.WriteLine("Двумерный массив, заполненный случайными вещественными числами, имеет следующий вид:");
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write($"{arr[i, j]:0.000} ");
+        Console.WriteLine();
+    }
+
+}
+
+int line = GetNumber("Задайте чсило строк m двумерного массива : ");
+int column = GetNumber("Задайте число столбцов n двумерного массива : ");
+double[,] rnd = ArrayRandom(line, column);
+PrintArray(rnd);
---
+++ task50/Program.cs
@@ -0,0 +1,61 @@
+﻿/*Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+
+
+
+int GetNumber(string message)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        string num = Console.ReadLine();
+        if (int.TryParse(num, out int number) == false)
+            Console.WriteLine("Введено не число");
+        else
+        {
+            if (number <= 0)
+                Console.WriteLine("Задайте позицию больше 0");
+            else
+                return number;
+        }
+    }
+}
+
+int[,] ArrayRandom()
+{
+    int[,] arr = new int[new Random().Next(2, 10), new Random().Next(2, 10)];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            arr[i, j] = rnd.Next(0, 100);
+    }
+    return arr;
+}
+
+void PositionElement(int[,] arr, int line, int column)
+{
+    Console.WriteLine("Двумерный массив имеет следующий вид:");
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write($"{arr[i, j]} ");
+        Console.WriteLine();
+    }
+    if (line <= (arr.GetLength(0) + 1) & line > 0 & column <= (arr.GetLength(1) + 1) & column > 0)
+        Console.WriteLine($"Значение элемента , занимающеего позицию [{line};{column}] равна:{arr[line - 1, column - 1]}");
+    else Console.WriteLine($"Такого элемента, , занимающеего позицию [{line};{column}] в массиве нет");
+
+}
+
+
+int line = GetNumber("Введите позицию элемента в строке : ");
+int column = GetNumber("Введите позицию элемента в столбце: ");
+PositionElement(ArrayRandom(), line, column);
